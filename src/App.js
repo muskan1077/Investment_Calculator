d64c4af891f1dc7557007d37811c25a2b9b96614
muskan1077 <muskan.1077gupta@gmail.com>
2023-08-10 18:05:24 +0530

    commit_message

--- src/App.js
+++ src/App.js
@@ -1,52 +1,43 @@
-//import logo from './assets/investment-calculator-logo.png';
 import { useState } from 'react';
 import Header from './component/Header/Header';
-import ResultTable from './component/ResultTable/ResultTable';
+import ResultsTable from './component/ResultTable/ResultTable';
 import UserInput from './component/UserInput/UserInput';
-function App() {
-    const[result,setResults] =  useState(null);
-  const calculateHandler = (userInput) => {
-    userInput(userInput);
-  };
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
 
-    const yearlyData = []; // per-year results
-    if(userInput){
-      let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
+function App() {
+  const [userInput, setUserInput] = useState(null);
+
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+
+  const yearlyData = [];
+
+  if (userInput) {
+    let currentSavings = +userInput['current-savings'];
+    const yearlyContribution = +userInput['yearly-contribution'];
     const expectedReturn = +userInput['expected-return'] / 100;
     const duration = +userInput['duration'];
 
-    // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
       yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
         year: i + 1,
         yearlyInterest: yearlyInterest,
         savingsEndOfYear: currentSavings,
         yearlyContribution: yearlyContribution,
       });
     }
-    
-      //setResuilts(yearlyData);
-    
-
-    // do something with yearlyData ...
-  };
+  }
 
   return (
     <div>
-      
-      <Header/>
-      <UserInput onCalculate = {calculateHandler} />
-      {/* Todo: Show below table conditionally (only once result data is available) */}
-      {/* Show fallback text if no data is available */}
-      {!userInput && <p>No investment calculated yet.</p>}
-      {userInput&&<ResultTable data ={yearlydata}/>}
+      <Header />
 
+      <UserInput onCalculate={calculateHandler} />
+
+      {!userInput && <p style={{textAlign: 'center'}}>No investment calculated yet.</p>}
+      {userInput && <ResultsTable data={yearlyData} initialInvestment={userInput['current-savings']} />}
     </div>
   );
 }
